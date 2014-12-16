ae5275fac1b7fc52d8d09270a2113822f34ff49e
Brendan McMahon <bmcmahon@seek.com.au>
2014-12-17 10:43:59 +1100

    started on dynamo session store

---
+++ Nancy.Session.DynamoDbBasedSessions/DynamoDbSessionStore.cs
@@ -0,0 +1,21 @@
+﻿namespace Nancy.Session
+{
+    public class DynamoDbSessionStore
+    {
+        private const string CreateDateKey = "CreateDate";
+        private const string ExpiresKey = "Expires";
+        private const string SessionDataKey = "SessionData";
+        private const string RecordFormatKey = "Ver";
+
+        private readonly string _sessionIdKey;
+        private readonly string _tableName;
+
+        public DynamoDbSessionStore(string sessionIdKey, string tableName)
+        {
+            _sessionIdKey = sessionIdKey;
+            _tableName = tableName;
+        }
+
+
+    }
+}
