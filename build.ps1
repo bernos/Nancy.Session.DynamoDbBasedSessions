c5de47b42ed7ceaadf3179202ebb71654711f79b
Brendan McMahon <bernos@gmail.com>
2014-12-16 21:38:39 +1100

    added license

---
+++ build.ps1
@@ -0,0 +1,26 @@
+Param(
+    [Parameter(Position=1,Mandatory=0)]
+    [string[]]$task_list = @(),
+    
+    [Parameter(Position=2,Mandatory=0)]
+    [string]$version
+)
+
+
+$configuration = "Release"
+
+$build_file = 'default.ps1'
+
+$projects = @(
+    "Nancy.Session.DynamoDbBasedSessions"
+)
+
+$properties = @{
+    "configuration" = $configuration;
+    "version"		= $version;
+    "projects"		= $projects;
+}
+
+import-module .\packages\psake.4.4.1\tools\psake.psm1
+
+invoke-psake $build_file $task_list -Properties $properties
