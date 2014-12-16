fc2479307e3adb82311a57fbcd04ae3570fa3bdd
Brendan McMahon <bernos@gmail.com>
2014-12-16 21:30:39 +1100

    initial commit

---
+++ Nancy.Session.DynamoDbBasedSessions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancy.Session.DynamoDbBasedSessions
+{
+    public class Class1
+    {
+    }
+}
