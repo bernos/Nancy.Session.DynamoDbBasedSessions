e8c4fd72c62af5b051ba8aa75d8dc306dda8f846
Brendan McMahon <bernos@gmail.com>
2014-12-22 20:51:04 +1100

    Added tests for DynamoDbSessionRecord

---
+++ Nancy.Session.DynamoDbBasedSessions.Tests/DynamoDbSessionRecordTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace Nancy.Session.Tests
+{
+    public class DynamoDbSessionRecordTests
+    {
+        [Fact]
+        [Trait("Category", "Unit tests")]
+        public void Should_Correctly_Determine_Expiry_Status()
+        {
+            var record = new DynamoDbSessionRecord("abc123", "may application", DateTime.UtcNow.AddSeconds(-1), "data",
+                DateTime.UtcNow);
+
+            Assert.True(record.HasExpired);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit tests")]
+        public void Should_Throw_For_Invalid_SessionId()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new DynamoDbSessionRecord("", "asdf", DateTime.UtcNow, "data", DateTime.UtcNow));
+        }
+
+        [Fact]
+        [Trait("Category", "Unit tests")]
+        public void Should_Throw_For_Invalid_ApplicationName()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new DynamoDbSessionRecord("asdf", "", DateTime.UtcNow, "data", DateTime.UtcNow));
+        }
+    }
+}
